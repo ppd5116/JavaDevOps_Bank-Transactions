719d0d6b27796afa23aac691a9ce7c41eb67b8cd
ppd5116 <48370769+ppd5116@users.noreply.github.com>
2021-07-18 23:33:04 -0500

    Update BankCustomerDemo.java

--- Java_BankTransaction/src/BankCustomerDemo.java
+++ Java_BankTransaction/src/BankCustomerDemo.java
@@ -3,7 +3,7 @@ public class BankCustomerDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BankCustomer customer1 = new BankCustomer();
+		/*BankCustomer customer1 = new BankCustomer();
 		customer1.setCustomerName("Pallavi D");
 		customer1.depositFunds(50);
 		customer1.withdrawFunds(20);
@@ -14,7 +14,52 @@ public class BankCustomerDemo {
 		customer2.depositFunds(0);
 		customer2.withdrawFunds(200);
 		customer2.setEmail("ppd5116@psu.edu");
-		customer2.displayCustomerDetails();
+		customer2.displayCustomerDetails();*/
+		System.out.println("Do you want to add new customers: ");
+		Scanner sc = new Scanner(System.in);
+		String ask = sc.nextLine();
+		switch(ask) {
+		case "yes": 
+			String answer; //only enter yes, y , no , or n as input to this variable
+			BankCustomer customer = new BankCustomer();
+			float balance, amount;
+			String name, email;
+			balance = sc.nextFloat();
+			name = sc.nextLine();
+			email = sc.nextLine();
+			customer.setBalance(balance);
+			customer.setCustomerName(name);
+			customer.setEmail(email);
+			
+			System.out.println("\nDo you want to deposit funds?");
+			answer = sc.nextLine();
+			if(answer == "yes" || answer == "y") {
+				System.out.println("\nEnter amount to deposit:");
+				amount = sc.nextFloat();
+				customer.depositFunds(amount);
+			}
+			
+			System.out.println("\nDo you want to withdraw funds?");
+			answer = sc.nextLine();
+			if(answer == "yes" || answer == "y") {
+				System.out.println("\nEnter amount to withdraw:");
+				amount = sc.nextFloat();
+				customer.withdrawFunds(amount);
+			}
+			
+			System.out.println("\nDo you want to display informations?");
+			answer = sc.nextLine();
+			if(answer == "yes" || answer == "y") {
+				customer.displayCustomerDetails();
+			}
+			
+			break;
+			
+		case "no":
+			System.out.println("\nLogging out of Bank Customer Records!!");
+			break;
+		default: System.out.println("Not a valid answer");
+		}
 	}
 	
 }
